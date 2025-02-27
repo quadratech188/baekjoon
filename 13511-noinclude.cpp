4c698539f42d753e2468b2d50fdbf61b54522410
Quadratech188 <quadratech188@gmail.com>
2025-02-28 02:50:01 +0900

    13511

---
+++ 13511-noinclude.cpp
@@ -0,0 +1,502 @@
+#include <cmath>
+#include <cstddef>
+#include <cstdio>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <ranges>
+#include <utility>
+#include <vector>
+
+template <typename Vertex, typename Edge>
+class ListGraph {
+public:
+	using index_t = int;
+	using vertex_t = Vertex;
+	using edge_t = Edge;
+
+private:
+	std::vector<Vertex> data;
+	std::vector<std::vector<std::pair<index_t, edge_t>>> connections;
+	int _size;
+
+public:
+
+	ListGraph(int size, const Vertex& defaultV = Vertex()): data(size, defaultV), connections(size), _size(size) {}
+	ListGraph(std::vector<Vertex>&& values):
+		data(std::move(values)),
+		connections(data.size()),
+		_size(data.size()) {}
+
+	index_t add(Vertex data) {
+		data.push_back(data);
+		connections.emplace_back();
+		_size += 1;
+		return _size - 1;
+	}
+
+	void reserve(int size) {
+		data.reserve(size);
+		connections.reserve(size);
+	}
+
+	void connect(index_t parent, index_t child, edge_t edge = edge_t()) {
+		connections[parent].push_back(std::make_pair(child, edge));
+	}
+
+	size_t size() {
+		return _size;
+	}
+
+	vertex_t& operator[](index_t index) {
+		return data[index];
+	}
+
+	class child {
+	public:
+		child(ListGraph* graph, index_t parent, int list_index):
+			graph(graph), parent(parent), list_index(list_index) {}
+
+		child():
+			graph(nullptr), list_index(0) {}
+
+		index_t index() {
+			return graph->connections[parent][list_index].first;
+		}
+		edge_t& edge() {
+			return graph->connections[parent][list_index].second;
+		}
+		vertex_t& value() {
+			return graph->data[index()];
+		}
+
+		operator index_t() {
+			return index();
+		}
+
+	private:
+		ListGraph* graph;
+		index_t parent;
+		int list_index;
+	};
+
+	auto children(index_t parent) {
+		return std::ranges::iota_view(static_cast<size_t>(0), connections[parent].size())
+			| std::views::transform([this, parent](index_t index) {
+					return child(this, parent, index);
+					});
+	}
+};
+
+template <typename G>
+class TreeWrapper {
+public:
+	using index_t = G::index_t;
+	using vertex_t = G::vertex_t;
+	using edge_t = G::edge_t;
+
+private:
+	G& graph;
+	index_t root;
+	std::vector<index_t> parents;
+
+	void init(index_t parent, index_t before_parent) {
+		parents[parent] = before_parent;
+		for (auto it: graph.children(parent)) {
+			if (it == before_parent) continue;
+			init(it, parent);
+		}
+	}
+
+public:
+	TreeWrapper(G& graph, index_t root):
+		graph(graph), root(root), parents(graph.size()) {
+		init(root, root);
+	}
+
+	vertex_t& operator[](index_t index) {
+		return graph[index];
+	}
+
+	auto children(index_t parent) {
+		return graph.children(parent) | std::views::filter([this, parent](auto it) {
+				return it != parents[parent];
+				});
+	}
+
+	index_t parent(index_t child) {
+		return parents[child];
+	}
+};
+
+struct None {};
+
+template<typename T, typename T2 = T>
+struct Vec2 {
+	using type = T;
+	
+	T x, y;
+
+	Vec2(T x, T y): x(x), y(y) {}
+	Vec2(): x(T()), y(T()) {}
+
+	Vec2 operator+(const Vec2& other) const {
+		return Vec2(this->x + other.x, this->y + other.y);
+	}
+
+	Vec2 operator-(const Vec2& other) const {
+		return Vec2(this->x - other.x, this->y - other.y);
+	}
+
+	template<typename D>
+	Vec2<D> operator/(const D other) const {
+		return Vec2<D>(this->x / other, this->y / other);
+	}
+
+	Vec2<T2> operator*(const T other) const {
+		return Vec2<T2>((T2)this->x * other, (T2)this->y * other);
+	}
+
+	bool operator<(const Vec2& other) const {
+		if (this->x != other.x) return this->x < other.x;
+		return this->y < other.y;
+	}
+
+	bool operator==(const Vec2& other) const {
+		return this->x == other.x && this->y == other.y;
+	}
+
+	bool operator!=(const Vec2& other) const {
+		return this->x != other.x || this->y != other.y;
+	}
+
+	Vec2 rotate(const double angle) const {
+		return Vec2(
+				x * std::cos(angle) - y * std::sin(angle),
+				x * std::sin(angle) + y * std::cos(angle)
+				);
+	}
+
+	T2 dot(const Vec2& other) const {
+		return static_cast<T2>(this->x) * other.x + static_cast<T2>(this->y) * other.y;
+	}
+
+	T2 cross(const Vec2& other) const {
+		return static_cast<T2>(this->x) * other.y - static_cast<T2>(this->y) * other.x;
+	}
+
+	T2 size2() const {
+		return static_cast<T2>(this->x) * this->x + static_cast<T2>(this->y) * this->y;
+	}
+
+	T length() const {
+		return std::sqrt(size2());
+	}
+
+	double theta() const {
+		return std::atan2(y, x);
+	}
+
+	static Vec2 zero() {
+		return Vec2(0, 0);
+	}
+
+	static Vec2 one() {
+		return Vec2(1, 1);
+	}
+};
+
+template <typename T, typename T2>
+std::istream& operator>>(std::istream& is, Vec2<T, T2>& vec2) {
+	is >> vec2.x >> vec2.y;
+	return is;
+}
+
+typedef Vec2<int, long long int> Int2;
+typedef Vec2<double, double> Double2;
+
+template <typename V2>
+class Bounds2 {
+
+public:
+	Bounds2(V2 start, V2 end): left(start), right(end) {}
+	Bounds2(V2 end): left(V2::zero()), right(end) {}
+
+	struct iterator {
+		using value_type = V2;
+		using difference_type = std::ptrdiff_t;
+		using pointer = const V2*;
+		using reference = const V2&;
+		using iterator_category = std::forward_iterator_tag;
+
+		iterator(V2 pos, const Bounds2& bounds): pos(pos), bounds(bounds) {}
+		V2 operator*() {
+			return pos;
+		}
+
+		bool operator==(const iterator& other) {
+			return pos == other.pos;
+		}
+
+		bool operator!=(const iterator& other) {
+			return pos != other.pos;
+		}
+
+		iterator& operator++() {
+			pos.x ++;
+			if (pos.x == bounds.right.x) {
+				pos.x = bounds.left.x;
+				pos.y ++;
+			}
+			return *this;
+		}
+		
+	private:
+		V2 pos;
+		const Bounds2& bounds;
+	};
+
+	iterator begin() const {
+		return iterator(left, *this);
+	}
+
+	iterator end() const {
+		return iterator({left.x, right.y}, *this); // so ugly
+	}
+
+	bool contains(V2 pos) const {
+		return left.x <= pos.x && pos.x < right.x
+			&& left.y <= pos.y && pos.y < right.y;
+	}
+
+	V2::type& x1() {
+		return left.x;
+	}
+	V2::type& x2() {
+		return right.x;
+	}
+	V2::type& y1() {
+		return left.y;
+	}
+	V2::type& y2() {
+		return right.y;
+	}
+
+private:
+	V2 left, right;
+};
+
+template <typename T>
+std::istream& operator>>(std::istream& is, Bounds2<T>& bounds2) {
+	is >> bounds2.lt() >> bounds2.rd();
+	return is;
+}
+
+typedef Bounds2<Int2> Range2;
+
+template <typename T>
+class Matrix {
+public:
+
+	Matrix(int columns, int rows, T defaultValue = T()):
+		_values(columns * rows, defaultValue), _size(columns, rows) {}
+
+	Matrix(): _size(Int2::zero()), _values() {}
+
+	T& operator()(int column, int row) {
+		return this->_values[row * _size.x + column];
+	}
+
+	const T& operator()(int column, int row) const {
+		return this->_values[row * _size.x + column];
+	}
+
+	T& operator[](Int2 index) {
+		return this->_values[index.y * _size.x + index.x];
+	}
+
+	bool operator==(const Matrix<T>& other) const {
+		return _size == other._size && _values == other._values;
+	}
+	
+	bool operator<(const Matrix<T>& other) const {
+		if (_size != other._size) return _size < other._size;
+
+		return _values < other._values;
+	}
+
+	Matrix operator*(const Matrix& other) {
+		Matrix result(other._size.x, _size.y);
+
+		for (Int2 index: result.bounds()) {
+			for (int depth = 0; depth < _size.x; depth++) {
+				result[index] += *this(depth, index.y) * *this(index.x, depth);
+			}
+		}
+
+		return result;
+	}
+
+	Int2 size() {
+		return _size;
+	}
+
+	Range2 bounds() {
+		return Range2(Int2::zero(), _size);
+	}
+
+	int rawIndex(Int2 index) const {
+		return index.y * _size.x + index.x;
+	}
+
+private:
+	std::vector<T> _values;
+	Int2 _size;
+};
+
+template<typename T>
+std::istream& operator>>(std::istream& input, Matrix<T>& matrix) {
+	for (Int2 index: matrix.bounds())
+		input >> matrix[index];
+
+	return input;
+}
+
+struct SparseTable {
+	Matrix<size_t> values;
+
+	SparseTable(): values(Matrix<size_t>()) {}
+
+	template <typename Callable>
+	SparseTable(size_t n, int iterations, Callable func) {
+		int sum = 0;
+
+		size_t depth = 1;
+		while (true) {
+			sum += (1 << (depth - 1));
+			if (sum >= iterations) break;
+			depth ++;
+		}
+
+		values = Matrix<size_t>(n, depth);
+
+		for (size_t i = 0; i < n; i++) {
+			values(i, 0) = func(i);
+		}
+
+		for (size_t row = 1; row < depth; row++) {
+			for (size_t i = 0; i < n; i++) {
+				size_t intermediate = values(i, row - 1);
+				values(i, row) = values(intermediate, row - 1);
+			}
+		}
+	}
+
+	size_t after(size_t index, int iterations) {
+		int i = 0;
+		size_t result = index;
+
+		for (size_t i = 0; iterations > 0; i++) {
+			if (iterations & 1) {
+				result = values(result, i);
+			}
+			iterations >>= 1;
+		}
+
+		return result;
+	}
+
+	size_t follow_until_same(size_t a, size_t b) {
+		if (a == b) return a;
+		for (int i = values.size().y - 1; i >= 0; i--) {
+			if (values(a, i) != values(b, i)) {
+				a = values(a, i);
+				b = values(b, i);
+			}
+		}
+		return values(a, 0);
+	}
+};
+
+inline void FastIO() {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	std::cout.tie(nullptr);
+}
+
+template <typename G>
+void calculate_depths_and_weights(G& graph, std::vector<long long int>& weights, int root, int depth = 0, long long int weight = 0) {
+	graph[root] = depth;
+	weights[root] = weight;
+
+	for (auto child: graph.children(root))
+		calculate_depths_and_weights(graph, weights, child, depth + 1, weight + child.edge());
+}
+
+template <typename G>
+int lca(G& graph, SparseTable& lookup, int a, int b) {
+	if (graph[a] > graph[b]) std::swap(a, b);
+
+	b = lookup.after(b, graph[b] - graph[a]);
+
+	return lookup.follow_until_same(a, b);
+}
+
+int main() {
+	// FastIO();
+	int n;
+	std::cin >> n;
+
+	ListGraph<int, int> graph(n);
+	std::vector<long long int> weights(n);
+
+	for (int i = 0; i < n - 1; i++) {
+		int u, v, w;
+		std::cin >> u >> v >> w;
+
+		graph.connect(u - 1, v - 1, w);
+		graph.connect(v - 1, u - 1, w);
+	}
+
+	TreeWrapper<decltype(graph)> tree(graph, 0);
+
+	SparseTable lookup(n, n - 1, [&tree](int child) {
+			return tree.parent(child);
+			});
+
+	calculate_depths_and_weights(tree, weights, 0);
+
+	int m;
+	std::cin >> m;
+	for (int i = 0; i < m; i++) {
+		char type;
+		std::cin >> type;
+		switch(type) {
+			case '1': {
+				int u, v;
+				std::cin >> u >> v;
+				std::cout << weights[u - 1] + weights[v - 1]
+					   - 2 * weights[lca(tree, lookup, u - 1, v - 1)] << '\n';
+				break;
+			}
+			case '2': {
+				int u, v, k;
+				std::cin >> u >> v >> k;
+				u -= 1;
+				v -= 1;
+				k -= 1;
+
+				int ancestor = lca(tree, lookup, u, v);
+				int first_half = graph[u] - graph[ancestor];
+				if (k <= first_half)
+					std::cout << lookup.after(u, k) + 1 << '\n';
+				else {
+					int length = graph[u] + graph[v]
+					       - 2 * graph[ancestor];
+
+					std::cout << lookup.after(v, length - k) + 1 << '\n';
+				}
+			}
+		}
+	}
+}
---
+++ 13511.cpp
@@ -0,0 +1,84 @@
+#include "modules/ListGraph.h"
+#include "modules/TreeWrapper.h"
+#include "modules/Types.h"
+#include "modules/SparseTable.h"
+#include "modules/FastIO.h"
+
+#include <iostream>
+
+template <typename G>
+void calculate_depths_and_weights(G& graph, std::vector<long long int>& weights, int root, int depth = 0, long long int weight = 0) {
+	graph[root] = depth;
+	weights[root] = weight;
+
+	for (auto child: graph.children(root))
+		calculate_depths_and_weights(graph, weights, child, depth + 1, weight + child.edge());
+}
+
+template <typename G>
+int lca(G& graph, SparseTable& lookup, int a, int b) {
+	if (graph[a] > graph[b]) std::swap(a, b);
+
+	b = lookup.after(b, graph[b] - graph[a]);
+
+	return lookup.follow_until_same(a, b);
+}
+
+int main() {
+	// FastIO();
+	int n;
+	std::cin >> n;
+
+	ListGraph<int, int> graph(n);
+	std::vector<long long int> weights(n);
+
+	for (int i = 0; i < n - 1; i++) {
+		int u, v, w;
+		std::cin >> u >> v >> w;
+
+		graph.connect(u - 1, v - 1, w);
+		graph.connect(v - 1, u - 1, w);
+	}
+
+	TreeWrapper<decltype(graph)> tree(graph, 0);
+
+	SparseTable lookup(n, n - 1, [&tree](int child) {
+			return tree.parent(child);
+			});
+
+	calculate_depths_and_weights(tree, weights, 0);
+
+	int m;
+	std::cin >> m;
+	for (int i = 0; i < m; i++) {
+		char type;
+		std::cin >> type;
+		switch(type) {
+			case '1': {
+				int u, v;
+				std::cin >> u >> v;
+				std::cout << weights[u - 1] + weights[v - 1]
+					   - 2 * weights[lca(tree, lookup, u - 1, v - 1)] << '\n';
+				break;
+			}
+			case '2': {
+				int u, v, k;
+				std::cin >> u >> v >> k;
+				u -= 1;
+				v -= 1;
+				k -= 1;
+
+				int ancestor = lca(tree, lookup, u, v);
+				int first_half = graph[u] - graph[ancestor];
+				if (k <= first_half)
+					std::cout << lookup.after(u, k) + 1 << '\n';
+				else {
+					int length = graph[u] + graph[v]
+					       - 2 * graph[ancestor];
+
+					std::cout << lookup.after(v, length - k) + 1 << '\n';
+				}
+			}
+		}
+	}
+}
