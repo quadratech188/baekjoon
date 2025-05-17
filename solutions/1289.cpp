818d41b994a72f221679ee8852725d07a7d89037
Quadratech188 <quadratech188@gmail.com>
2025-05-17 18:57:36 +0900

    cleaning up the mess

--- solutions/1289.cpp
+++ solutions/1289.cpp
@@ -2,15 +2,13 @@
 #include "../modules/TreeWrapper.h"
 #include "../modules/ModInt.h"
 #include "../modules/FastIO2.h"
-#include <cstdint>
 #include <ostream>
 #include <iostream>
 #include <unistd.h>
-#include <variant>
 
 sm32_1e9_7 traffic = 0;
 template <Graph G>
-void solve(G& tree, size_t root) {
+void solve(G& tree, uint root) {
 	sm32_1e9_7 sum = 0;
 	for (auto const& child: tree.children(root)) {
 		solve(tree, child);
@@ -22,21 +20,15 @@ void solve(G& tree, size_t root) {
 	tree[root] = sum + 1;
 }
 
-struct a {
-	uint32_t a;
-	uint32_t b;
-	std::monostate c;
-};
-
 int main() {
-	size_t n;
+	uint n;
 	Fast::cin >> n;
 
 	ListGraph<sm32_1e9_7, sm32_1e9_7>
-		::index<uint32_t> graph(n);
+		::index<uint> graph(n);
 
-	for (size_t i = 0; i < n - 1; i++) {
-		size_t a, b, w;
+	for (uint i = 0; i < n - 1; i++) {
+		uint a, b, w;
 		Fast::cin >> a >> b >> w;
 		graph.connect(a - 1, b - 1, sm32_1e9_7::verified(w));
 		graph.connect(b - 1, a - 1, sm32_1e9_7::verified(w));
