f2787e5345225c41d4d2f587ef018bcdfe95e62b
Quadratech188 <quadratech188@gmail.com>
2025-05-29 22:55:53 +0900

    feat: Rewrite SegmentTree

---
+++ modules/SegmentTree2.h
@@ -0,0 +1,105 @@
+#include <concepts>
+#include <functional>
+#include <ranges>
+
+#include "Segment.h"
+#include "DummyIterator.h"
+
+template <typename T>
+concept SegmentTreeElement = requires(T x, T l, T r) {
+	{l + r} -> std::same_as<T>;
+	{x.reinit((T const&)l, (T const&)r)};
+};
+
+template <typename T>
+class SegmentTree {
+public:
+	using value_type = T;
+
+	template <std::ranges::range R>
+	SegmentTree(R&& range):
+		_size(std::ranges::size(range)), _values(4 * _size) {
+		auto it = std::ranges::begin(range);
+		init(Segment(0, _size), 0, it);
+	}
+
+	template <typename Extractor = std::identity>
+	auto sum(Segment segment, Extractor extractor = {}) {
+		return sum(segment, Segment(0, _size), 0, extractor);
+	}
+
+	template <typename Extractor = std::identity>
+	auto sum(size_t start, size_t end, Extractor extractor = {}) {
+		return sum(Segment(start, end), extractor);
+	}
+
+	template <typename Callable>
+	void update(size_t index, Callable func) {
+		return update(index, 0, Segment(0, _size), func);
+	}
+
+	constexpr size_t size() const noexcept {
+		return _size;
+	}
+
+	constexpr T const& root() const noexcept {
+		return _values[0];
+	}
+private:
+	size_t const _size;
+	std::vector<T> _values;
+
+	template <typename Iter>
+	void init(Segment segment, size_t index, Iter& iterator) {
+		if (segment.size() == 1) {
+			_values[index] = T(*iterator);
+			++iterator;
+			return;
+		}
+
+		size_t left = 2 * index + 1;
+		size_t right = 2 * index + 2;
+
+		init(segment.left(), left, iterator);
+		init(segment.right(), right, iterator);
+
+		_values[index] = _values[left] + _values[right];
+	}
+
+	template <typename Extractor>
+	auto sum(Segment const query, Segment const segment, size_t const index, Extractor extractor) {
+		if (query.includes(segment))
+			return std::invoke(extractor, static_cast<T const&>(_values[index]));
+
+		size_t const left = index * 2 + 1;
+		size_t const right = index * 2 + 2;
+
+		if (segment.center() <= query.start)
+			return sum(query, segment.right(), right, extractor);
+
+		if (query.end <= segment.center())
+			return sum(query, segment.left(), left, extractor);
+
+		return sum(query, segment.left(), left, extractor)
+		     + sum(query, segment.right(), right, extractor);
+	}
+
+	template <typename Callable>
+	void update(size_t const index, size_t const value_index, Segment const segment, Callable func) {
+		if (segment.size() == 1) {
+			std::invoke(func, _values[value_index]);
+			return;
+		}
+
+		size_t const left = value_index * 2 + 1;
+		size_t const right = value_index * 2 + 2;
+
+		if (index < segment.center())
+			update(index, left, segment.left(), func);
+
+		if (segment.center() < index)
+			update(index, right, segment.right(), func);
+
+		_values[value_index].reinit(_values[left], _values[right]);
+	}
+};
