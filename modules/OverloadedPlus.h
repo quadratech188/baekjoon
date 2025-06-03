6c9300fbfb05246b0e818788775bba727a953acb
Quadratech188 <quadratech188@gmail.com>
2025-06-03 14:56:00 +0900

    feat: Make OverloadedPlus compatible with SegmentTree

--- modules/OverloadedPlus.h
+++ modules/OverloadedPlus.h
@@ -24,6 +24,9 @@ public:
 	OverloadedPlus operator+(OverloadedPlus const& other) {
 		return op(value, other.value);
 	}
+	void reinit(OverloadedPlus const& l, OverloadedPlus const& r) {
+		value = op(l, r);
+	}
 };
 
 template <typename T>
