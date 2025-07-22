82a3f0b8c8e8039ba292ee8486be68d1c2aeb6bb
Quadratech188 <quadratech188@gmail.com>
2025-07-23 02:21:17 +0900

    feat: Solve 12015 & 14003

---
+++ solutions/14003.cpp
@@ -0,0 +1,51 @@
+#include "../modules/FastIO2.h"
+#include "../modules/Output.h"
+#include <algorithm>
+#include <iterator>
+
+int main() {
+	uint n;
+	Fast::cin >> n;
+
+	auto values = Fast::cin.to_vec<int>(n);
+
+	std::vector<uint> insert_positions(n);
+
+	std::vector<int> lis;
+	uint peak;
+
+	lis.push_back(values[0]);
+	insert_positions[0] = 0;
+
+	uint max_length = 0;
+
+	for (uint i = 1; i < n; i++) {
+		int val = values[i];
+		do {
+			if (lis[lis.size() - 1] < val) {
+				insert_positions[i] = lis.size();
+				lis.push_back(val);
+				continue;
+			}
+
+			auto ptr = std::ranges::lower_bound(lis, val);
+			insert_positions[i] = ptr - lis.begin();
+			*ptr = val;
+		} while (false);
+
+		if (lis.size() > max_length) {
+			max_length = lis.size();
+			peak = i;
+		}
+
+	}
+
+	uint target_pos = insert_positions[peak];
+	for (uint i = peak; i <= peak; i--) {
+		if (insert_positions[i] == target_pos) {
+			lis[insert_positions[i]] = values[i];
+			target_pos --;
+		}
+	}
+	std::cout << lis.size() << '\n' << lis;
+}
