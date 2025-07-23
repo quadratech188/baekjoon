f363fd24ea78e424f83806b072e2ca4325c810ad
Quadratech188 <quadratech188@gmail.com>
2025-07-23 21:37:14 +0900

    feat: Add home-baked cpp-merge implementation

---
+++ bin/cpp-merge.py
@@ -0,0 +1,23 @@
+import pathlib
+import re
+
+PRAGMA_ONCE_REGEX = re.compile('#pragma once')
+INCLUDE_QUOTES_REGEX = re.compile('#include "(?P<path>.+)"')
+
+visited_headers = set()
+def parse_file(filepath: pathlib.Path):
+    if filepath in visited_headers: return
+
+    with open(filepath, mode='r') as f:
+        for line in f.readlines():
+            if PRAGMA_ONCE_REGEX.match(line): continue
+
+            match = INCLUDE_QUOTES_REGEX.match(line);
+            if match is not None:
+                parse_file(filepath.parent.joinpath(match.group('path')))
+            else:
+                print(line, end='')
+
+import sys
+
+parse_file(pathlib.Path(sys.argv[1]))
