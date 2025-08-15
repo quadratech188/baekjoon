f2a32235a50f24399cc337b9f0dbad9c4cd9e6c3
Quadratech188 <quadratech188@gmail.com>
2025-08-16 06:00:17 +0900

    feat: Update 2769 cheese

--- solutions/2769.py
+++ solutions/2769.py
@@ -1,5 +1,5 @@
 for m in range(int(input())):
- s=''.join(list(filter(lambda c: c.isalpha() and c.islower() or c in '|&^~()', input())))
+ s = input()
  v={c for c in s if c.isalpha()}
  h=''.join(f'{c}=1\n' for c in v)
  for i in range(len(s)):
@@ -12,13 +12,14 @@ for m in range(int(input())):
    continue
   else:
    break
+ print(f'Data set {m + 1}: ',end='')
  for u in (format(i,f'0{len(v)}b') for i in range(len(v))):
   h=''.join(f'{c}={u[j]}\n' for j,c in enumerate(v))
   q={}
   exec(h+'v1='+l,{},q)
   exec(h+'v2='+r,{},q)
   if q['v1']!=q['v2']:
-   print(f'Data set {m + 1}: Different')
+   print(f'Different')
    break
  else:
-  print(f'Data set {m + 1}: Equivalent')
+  print(f'Equivalent')
